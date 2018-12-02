d4abf3d947f64157451fb85d8cbb22b31223eabe
Erik brakke <eriksbrakke@gmail.com>
2018-12-02 08:54:57 -0500

    Day1 done

--- 2018/src/Day1.elm
+++ 2018/src/Day1.elm
@@ -1,8 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div)
-import Html.Attributes exposing (class)
+import Debug
+import Html exposing (Html, div, input, label, text)
+import Html.Attributes as Attrs exposing (class)
+import Html.Events as Evt
+import Set exposing (Set)
 
 
 main =
@@ -14,22 +17,25 @@ main =
         }
 
 
-type Model
-    = Nothing
+type alias Model =
+    { puzzleInput : String
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Nothing, Cmd.none )
+    ( Model "", Cmd.none )
 
 
 type Msg
-    = Noop
+    = Change String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Change str ->
+            ( { model | puzzleInput = str }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -39,4 +45,85 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ] []
+    div [ class "container" ]
+        [ div []
+            [ label [ Attrs.for "puzzleInput" ] []
+            , input [ Evt.onInput Change, Attrs.value model.puzzleInput, Attrs.name "puzzleInput" ] []
+            ]
+        , div []
+            [ text "Answer to question 1 is: "
+            , text <| String.fromInt <| solve1 model.puzzleInput
+            ]
+        , div []
+            [ text "Answer to question 2 is: " ]
+        , text <| String.fromInt <| solve2 model.puzzleInput
+        ]
+
+
+
+-- Helpers
+
+
+parseInput : String -> List Int
+parseInput input =
+    let
+        tokens =
+            String.split " " <| String.trim input
+    in
+    List.map tokenizeDelta tokens
+
+
+tokenizeDelta : String -> Int
+tokenizeDelta str =
+    let
+        split =
+            String.split "" str
+    in
+    case split of
+        "+" :: xs ->
+            Maybe.withDefault 0 <| String.toInt <| String.join "" xs
+
+        "-" :: xs ->
+            (*) -1 <| Maybe.withDefault 0 <| String.toInt <| String.join "" xs
+
+        _ ->
+            0
+
+
+solve1 : String -> Int
+solve1 input =
+    parseInput >> List.sum <| input
+
+
+solve2 : String -> Int
+solve2 input =
+    let
+        parsed =
+            parseInput input
+    in
+    sumAndCheckFrequency parsed (Set.fromList [ 0 ]) 0 parsed 0
+
+
+
+-- Note, this can recurse if the initial input does not actually terminate!
+
+
+sumAndCheckFrequency : List Int -> Set Int -> Int -> List Int -> Int -> Int
+sumAndCheckFrequency restInput frequency sum allInput loops =
+    case restInput of
+        [] ->
+            sumAndCheckFrequency allInput frequency sum allInput (loops + 1)
+
+        x :: xs ->
+            let
+                newSum =
+                    sum + x
+
+                newFrequency =
+                    Set.insert newSum frequency
+            in
+            if Set.member newSum frequency then
+                newSum
+
+            else
+                sumAndCheckFrequency xs newFrequency newSum allInput (loops + 1)
